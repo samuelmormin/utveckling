3f77cb06b166e99a5ed246fb04219fd2f2258c2c
samuelmormin <samuel.mormin@hetic.net>
2017-04-07 06:38:06 +0200

    :sparkles: Nos services/Process/Projets/A propos/footer

--- views/pages/home.php
+++ views/pages/home.php
@@ -8,13 +8,84 @@
     </div>
     <div class="page-intro">
         <h1>
-            NOUS VOUS ACCOMPAGNONS DANS LA
+            UTVECKLING VOUS ACCOMPAGNE DANS LA
             <br><br>
             <strong>CONCEPTION</strong> DE VOS <strong>PROJETS DIGITAUX</strong>
         </h1>
         <p class="tags">
             [ DÉVELOPPEMENT WEB ] [ MARKETING DIGITAL ] [ DESIGN ] [ E-COMMERCE ]
         </p>
-        <a class="button-savoir-plus" href="#">EN SAVOIR PLUS</a>
+        <div class="intro-actions">
+            <a class="button-contacter-intro" href="#">NOUS CONTACTER</a>
+            <a class="button-savoir-plus-intro" href="#">EN SAVOIR PLUS</a>
+        </div>
     </div>
+</section>
+<section class="home-services">
+    <h2>[ NOS SERVICES ]</h2>
+    <div class="service-description">
+        <img src="assets/img/dev_icon.png" alt="Développement web">
+        <h3>DÉVELOPPEMENT WEB</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+    <div class="service-description">
+        <img src="assets/img/marketing_icon.png" alt="Développement web">
+        <h3>MARKETING DIGITAL</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+    <div class="service-description">
+        <img src="assets/img/design_icon.png" alt="Développement web">
+        <h3>DESIGN</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+    <div class="service-description">
+        <img src="assets/img/ecommerce_icon.png" alt="Développement web">
+        <h3>E-COMMERCE</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+    <a class="button-savoir-plus" href="#">EN SAVOIR PLUS</a>
+</section>
+<section class="home-processus">
+    <h2>[ NOTRE PROCESSUS DE CRÉATION ]</h2>
+    <a class="button-savoir-plus" href="#">EN SAVOIR PLUS</a>
+</section>
+<section class="home-projets">
+    <h2>[ NOS DERNIES PROJETS ]</h2>
+    <div class="project-block">
+        <img src="assets/img/project.png" alt="projet-1">
+        <a class="button-savoir-plus" href="#">VOIR CE PROJET</a>
+    </div>
+    <div class="project-block">
+        <img src="assets/img/project.png" alt="projet-1">
+        <a class="button-savoir-plus" href="#">VOIR CE PROJET</a>
+    </div>    
+</section>
+<section class="home-apropos">
+    <h2>[ À PROPOS DE NOUS ]</h2>
+    <p>
+        Utveckling a été fondée par deux jeunes entrepeneurs en veille et en évolution constante
+        <br>
+        dans le domaine des technologies de l’information  de la communication.
+        <br><br>
+        Nous sommes une entreprise à taille humaine et nos clients le ressentent dans notre démarche
+        <br>
+        et notre volonté de les accompagner dans toutes les étapes d’un projet, de l’élaboration du cahier des charges à la réalisation.
+        <br><br>
+        Nous avons pour objectif de fournir à nos clients des projets sur-mesure, 
+        de qualité et optimisés afin de leur garantir un bon retour sur investissement.
+    </p>
+</section>
+<section class="home-outils">
+    
+</section>
+<section class="home-contacts">
+    
 </section>
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,9 @@
+    <section class="newsletter">
+        <h2>[ NEWSLETTER ]</h2>
+        <a class="button-savoir-plus" href="#">VALIDER</a>
+    </section>  
     <footer>
-        
+        <p>©Copyright 2016 - 2017 Utveckling | All Rights Reserved | Mention légales</p>
     </footer>
 </body>
 </html>
